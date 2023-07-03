06d3017d3988042dd9aadf618395aa315395b3b4
Juan Gallardo <jhgallardo99@gmail.com>
2023-07-02 19:16:25 -0600

    Arreglar problemas con el merge

--- client/src/components/CalendarTeacher.js
+++ client/src/components/CalendarTeacher.js
@@ -25,7 +25,7 @@ import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
-import { Toast } from 'primereact/toast';
+import { Toast } from "primereact/toast";
 
 // constantes
 import { SOLICITUDES_TIPOS_ARRAY } from "../constants/solicitudes";
@@ -48,7 +48,6 @@ import { EnviaNotificacione } from "../services/NotificacionesServices";
 moment.locale("es");
 moment.tz.setDefault("America/El _Salvador");
 const localizer = momentLocalizer(moment);
-
 
 class CalendarAlt extends React.Component {
   static contextType = ContextUsuario;
@@ -190,7 +189,9 @@ class CalendarAlt extends React.Component {
           element.end = new Date(element.end);
         });
 
-        this.setState({ events: json.filter(soli => soli.estado === 'PENDIENTE') });
+        this.setState({
+          events: json.filter((soli) => soli.estado === "PENDIENTE"),
+        });
       }
     } catch (error) {
       console.error(error);
@@ -296,9 +297,11 @@ class CalendarAlt extends React.Component {
         // localStorage.setItem("cachedEvents", JSON.stringify(events));
         this.setState({ events });
 
-        this.showSuccess('El horario ha sido registrado con éxito')
+        this.showSuccess("El horario ha sido registrado con éxito");
       } else {
-        this.showError('Ha ocurrido un error al momento de registrar el horario')
+        this.showError(
+          "Ha ocurrido un error al momento de registrar el horario"
+        );
       }
     } catch (error) {
       console.error(error);
@@ -337,13 +340,12 @@ class CalendarAlt extends React.Component {
       response
     );
 
-    if(response.status === 200){
-      this.envioNotificacionModifica(email)
-      this.showSuccess('El evento ha sido actualizado con éxito')
+    if (response.status === 200) {
+      this.envioNotificacionModifica(email);
+      this.showSuccess("El evento ha sido actualizado con éxito");
     } else {
-      this.showError('Ha ocurrido un error al actualizar el evento')
+      this.showError("Ha ocurrido un error al actualizar el evento");
     }
-
   }
 
   //  filters out specific event that is to be deleted and set that variable to state
@@ -367,12 +369,12 @@ class CalendarAlt extends React.Component {
       "🚀 ~ file: CalendarAlt.js:301 ~ CalendarAlt ~ response ~ response:",
       response
     );
-    
-    if(response.status === 200) {
-      this.envioNotificacionRechazo(email)
-      this.showSuccess('El evento ha sido eliminado con éxito')
+
+    if (response.status === 200) {
+      this.envioNotificacionRechazo(email);
+      this.showSuccess("El evento ha sido eliminado con éxito");
     } else {
-      this.showError('Ha ocurrido un error al eliminar el evento')
+      this.showError("Ha ocurrido un error al eliminar el evento");
     }
   }
 
@@ -421,85 +423,46 @@ class CalendarAlt extends React.Component {
     } catch (error) {
       console.error(error);
     }
-  }; 
+  };
 
   actualizarSolicitudEstado = async (estado) => {
     try {
-      console.log('evento a actualizar su estado: ', this.state.events);
+      console.log("evento a actualizar su estado: ", this.state.events);
 
       const body = {
         id_solicitud: this.state.events[0].id,
-        estado: estado
-      }
- 
-      const response = await actualizaEstadoSolicitud(body).catch(err => {
+        estado: estado,
+      };
+
+      const response = await actualizaEstadoSolicitud(body).catch((err) => {
         console.error(err);
-      })
-      console.log("🚀 ~ file: CalendarTeacher.js:420 ~ CalendarAlt ~ response ~ response:", response)
+      });
+      console.log(
+        "🚀 ~ file: CalendarTeacher.js:420 ~ CalendarAlt ~ response ~ response:",
+        response
+      );
 
-      if(response.status === 200) {
+      if (response.status === 200) {
+        this.showSuccess("La solicitud ha sido respondida");
+        this.getSolicitudesByIdUsuarioIdMateria();
 
-        this.showSuccess('La solicitud ha sido respondida')
-        this.getSolicitudesByIdUsuarioIdMateria()
-  
         const { email } = this.state;
-  
+
         switch (estado) {
-          case 'RECHAZADO':
-            this.envioNotificacionRechazo(email)
+          case "RECHAZADO":
+            this.envioNotificacionRechazo(email);
             break;
-        
+
           default:
             break;
         }
-
       } else {
-        this.showError('Ha ocurrido un error al responder la solicitud')
+        this.showError("Ha ocurrido un error al responder la solicitud");
       }
- 
     } catch (error) {
       console.error(error);
     }
-  }
-  actualizarSolicitudEstado = async (estado) => {
-    try {
-      console.log('evento a actualizar su estado: ', this.state.events);
-
-      const body = {
-        id_solicitud: this.state.events[0].id,
-        estado: estado
-      }
-
-      const response = await actualizaEstadoSolicitud(body).catch(err => {
-        console.error(err);
-      })
-      console.log("🚀 ~ file: CalendarTeacher.js:420 ~ CalendarAlt ~ response ~ response:", response)
-
-      if(response.status === 200) {
-
-        this.showSuccess('La solicitud ha sido respondida')
-        this.getSolicitudesByIdUsuarioIdMateria()
-  
-        const { email } = this.state;
-  
-        switch (estado) {
-          case 'RECHAZADO':
-            this.envioNotificacionRechazo(email)
-            break;
-        
-          default:
-            break;
-        }
-
-      } else {
-        this.showError('Ha ocurrido un error al responder la solicitud')
-      }
-
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
+  };
 
   concurrentEventExists = (slotInfo) => {
     return this.state.events.some(
@@ -564,17 +527,6 @@ class CalendarAlt extends React.Component {
     return (
       <div id="Calendar">
         <Toast ref={(el) => (this.toast = el)} />
-        <div className="flex w-full justify-content-end mb-5 lg:mb-0">
-          <Dropdown
-            value={this.state.materiaSeleccionada}
-            options={this.state.materias}
-            optionLabel="nombre"
-            onChange={(e) => this.setState({ materiaSeleccionada: e.value })}
-            placeholder="Seleccione una materia"
-            emptyMessage="No hay datos"
-            className="lg:hidden"
-          />
-        </div>
 
         <Box
           sx={{ minWidth: 120, maxWidth: "80%" }}
@@ -851,7 +803,7 @@ class CalendarAlt extends React.Component {
               label="Delete"
               secondary={"true"}
               onClick={() => {
-                this.actualizarSolicitudEstado('RECHAZADO'), this.handleClose();
+                this.actualizarSolicitudEstado("RECHAZADO"), this.handleClose();
               }}
             >
               rechazar
@@ -869,7 +821,7 @@ class CalendarAlt extends React.Component {
               label="Confirm Edit"
               secondary={"true"}
               onClick={() => {
-                this.actualizarSolicitudEstado('AUSENTE'), this.handleClose();
+                this.actualizarSolicitudEstado("AUSENTE"), this.handleClose();
               }}
             >
               AUSENTE
@@ -879,7 +831,7 @@ class CalendarAlt extends React.Component {
               secondary={"true"}
               onClick={() => {
                 // this.updateEvent(), this.handleClose();
-                this.actualizarSolicitudEstado('ATENDIDO'), this.handleClose();
+                this.actualizarSolicitudEstado("ATENDIDO"), this.handleClose();
               }}
             >
               ATENDIDO
