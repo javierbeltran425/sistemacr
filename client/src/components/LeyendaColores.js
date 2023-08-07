29269349902126080661fb14ec58e7901338d301
javierbeltran425 <00019315@uca.edu.sv>
2023-08-07 08:38:30 -0600

    agregando leyenda

--- client/src/components/LeyendaColores.js
+++ client/src/components/LeyendaColores.js
@@ -6,6 +6,13 @@ const LeyendaColores = () => {
             <p className='m-0 p-0 text-orange-500 text-right'>Manual de códigos de colores</p>
             <div className='flex gap-2'>
                 <div className='flex align-items-center gap-2'>
+                    <p>DISPONIBLE</p>
+                    <span style={{
+                        backgroundColor: '#caf1d8', width: 14
+                        , height: 14, display: 'inline-block'
+                    }}></span>
+                </div>
+                <div className='flex align-items-center gap-2'>
                     <p>ACEPTADA</p>
                     <span style={{
                         backgroundColor: '#17594a', width: 14
